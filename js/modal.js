19f94fc92621fe823cd62ba87e4769b33c4a0fd7
Leonie Viau <viau.leonie@orange.fr>
2022-12-09 17:15:49 +0100

    initial commit

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form
+close.addEventListener("click", closeModal => {
+  modalbg.style.display = "none";
+});
+
