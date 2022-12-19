f4e883630dd0233c41fed2fbd7be8839a68eafc1
Leonie Viau <viau.leonie@orange.fr>
2022-12-19 13:47:15 +0100

    modal ok

--- js/modal.js
+++ js/modal.js
@@ -42,9 +42,9 @@ const location4 = document.getElementById('location4');
 const location5 = document.getElementById('location5');
 const location6 = document.getElementById('location6');
 
-const dateFormat = /^\d{2}[./-]\d{2}[./-]\d{4}$/;
+const dateFormat = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
 const numbers = /^[0-9]+$/;
-const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 // submit handler
 
@@ -54,20 +54,21 @@ form.addEventListener('submit', (e) => {
 })
 
 function validate() {
-  if (first.value === '' && first.length > 1) {
-    alert ("Remplir le champ avec votre Prénom");
+  if (first.value === ''|| first.value.length <= 2) {
+    alert ("Veuillez entrer 2 caractères ou plus pour le champ du prénom");
     return false;
   } 
-  else if (last.value === '' && last.length > 1) { 
-    alert ("Remplir le champ avec votre Nom");
+  else if (last.value === '' || last.value.length <= 2) { 
+    alert ("Veuillez entrer 2 caractères ou plus pour le champ du nom");
     return false;
   } 
-  else if (emailFormat.test(email.value)) { 
+  else if (!emailFormat.test(email.value)) { 
     alert ("Remplir le champ avec votre email");
     return false;
   }
-  else if (dateFormat.test(birthdate.value)) { 
-    alert ("Remplir le champ avec votre date de naissance");
+  else if (!dateFormat.test(birthdate.value)) { 
+    console.log(1);
+    alert ("Vous devez entrer votre date de naissance");
     return false;
   }
   else if (!quantity.value.match(numbers)) { 
@@ -75,7 +76,7 @@ function validate() {
     return false;
   }
   else if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) { 
-    alert ("Choisir une ville");
+    alert ("Vous devez choisir une option");
     return false;
   }
 
