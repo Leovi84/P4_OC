e512cf977db93c61890b5b2b6414d85add1b774c
Leonie Viau <viau.leonie@orange.fr>
2022-12-24 11:38:35 +0100

    errors.push

--- js/modal.js
+++ js/modal.js
@@ -27,57 +27,68 @@ close.addEventListener("click", closeModal => {
   modalbg.style.display = "none";
 });
 
-// Valid on submit
 const form = document.querySelector('form');
 const input = document.getElementsByTagName('input');
-const first = document.getElementById('first');
-const last = document.getElementById('last');
-const email = document.getElementById('email');
-const birthdate = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
-const location1 = document.getElementById('location1');
-const location2 = document.getElementById('location2');
-const location3 = document.getElementById('location3');
-const location4 = document.getElementById('location4');
-const location5 = document.getElementById('location5');
-const location6 = document.getElementById('location6');
-
-const dateFormat = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
-const numbers = /^[0-9]+$/;
-const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-
-// submit handler
 
 form.addEventListener('submit', (e) => {
-  // on form submission, prevent default
   e.preventDefault();
 })
 
-function validate() {
-  if (first.value === ''|| first.value.length <= 2) {
-    alert ("Veuillez entrer 2 caractères ou plus pour le champ du prénom");
-    return false;
-  } 
-  else if (last.value === '' || last.value.length <= 2) { 
-    alert ("Veuillez entrer 2 caractères ou plus pour le champ du nom");
-    return false;
-  } 
-  else if (!emailFormat.test(email.value)) { 
-    alert ("Remplir le champ avec votre email");
-    return false;
-  }
-  else if (!dateFormat.test(birthdate.value)) { 
-    console.log(1);
-    alert ("Vous devez entrer votre date de naissance");
-    return false;
-  }
-  else if (!quantity.value.match(numbers)) { 
-    alert ("Indiquer le nombre de tournois");
-    return false;
-  }
-  else if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) { 
-    alert ("Vous devez choisir une option");
-    return false;
-  }
+  const first = document.getElementById('first');
+  const last = document.getElementById('last');
+  const email = document.getElementById('email');
+  const birthdate = document.getElementById('birthdate');
+  const quantity = document.getElementById('quantity');
+  const location1 = document.getElementById('location1');
+  const location2 = document.getElementById('location2');
+  const location3 = document.getElementById('location3');
+  const location4 = document.getElementById('location4');
+  const location5 = document.getElementById('location5');
+  const location6 = document.getElementById('location6');
 
+  const numbers = /^[0-9]+$/;
+  const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  const errorFirst = [];
+  const errorLast = [];
+  const errorEmail = [];
+  const errorBirthdate = [];
+  const errorQuantity = [];
+  const errorLocation = [];
+  const errorCheck = [];
+
+function validate() {
+
+  if (first.value === '' || first.length <= 2) {
+    errorFirst.push ("Veuillez entrer 2 caractères ou plus pour le champ du prénom");
+    return false;
+  } 
+  else if (last.value === '' || last.length <= 2) {
+    errorLast.push ("Veuillez entrer 2 caractères ou plus pour le champ du nom");
+    return false;
+  } 
+  else if (!emailFormat.test(email)) {
+    errorEmail.push ("Remplir le champ avec votre email");
+    return false;
+  }
+  else if (birthdate.value === '') {
+    errorBirthdate.push ("Vous devez entrer votre date de naissance");
+    return false;
+  }
+  else if (!quantity.value.match(numbers)) {
+    errorQuantity.push ("Indiquer le nombre de tournois");
+    return false;
+  }
+  else if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
+    errorLocation.push ("Vous devez choisir une option");
+    return false;
+  }
+  else if (!checkbox1.checked) {
+    errorCheck.push ("Vous devez cocher la case");
+    return false; 
+  }
+  else {
+    alert ("Merci ! Votre réservation a été reçue.");
+    return true;
+  }
 }
